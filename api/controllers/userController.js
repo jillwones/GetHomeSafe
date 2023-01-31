84f9432a6146265c20b33b4daf6105a1a3c24160
Andy <ajscottlewis@gmail.com>
2023-01-31 18:56:22 +0000

    Remove console.log message, add comment re returning email/user_id in signupUser method

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -24,13 +24,14 @@ const loginUser = async (req, res) => {
 // signup a user
 const signupUser = async (req, res) => {
   const {name, email, password} = req.body
-  console.log(req.body);
+
   try {
     const user = await User.signup(name, email, password)
 
     // create a token
     const token = createToken(user._id)
 
+    // if 'email' is changed to 'user_id: user._id', email is still passed in the response, not user_id
     res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
