e2fc13313c642ae8de75612e477ea9b3c73b594c
Andy Scott-Lewis <ajscottlewis@gmail.com>
2023-02-01 12:37:47 +0000

    Move homescreen code to settings screen, replace with basic display
    Co-authored-by: Eliza Thomas <elizamthomas1994@gmail.com>
    
    Co-authored-by: Bakhta Elamar <belamar75@gmail.com>

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -4,63 +4,11 @@ import { StyleSheet, Text, View, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function HomeScreen({ navigation }) {
-  const [token, setToken] = useState(null);
-  // const [userId, setUserId] = useState(null);
-  
-  useEffect(() => {
-    const retrieveToken = async () => {
-      const value = await AsyncStorage.getItem('token');
-      setToken(value);
-    };
-    retrieveToken();
-
-    // const retrieveUserId = async () => {
-    //   const value = await AsyncStorage.getItem('user_id');
-    //   setUserId(value);
-    // };
-    // retrieveUserId();
-
-    const changedScreen = navigation.addListener('focus', () => {
-      retrieveToken();
-    });
-
-    console.log('useEffect triggered');
-  }, [token]);
-
-  const handleLogout = async () => {
-    await AsyncStorage.removeItem('token');
-    setToken(null);
-    // await AsyncStorage.removeItem('user_id');
-    // setUserId(null);
-  };
 
   return (
     <View style={styles.globalContainer}>
-      {/* {token && <Text>Logged in as {userId}</Text>} */}
       <Text style={styles.title}>Get Home Safe</Text>
-      <View style={styles.homeButtonsContainer}>
-        {!token &&
-          <View style={styles.button}>
-            <Pressable onPress={() => navigation.navigate('Login')}>
-              <Text style={styles.inputLabel}>Log in</Text>
-            </Pressable>
-          </View>
-        }
-        {!token &&
-          <View style={styles.button}>
-            <Pressable onPress={() => navigation.navigate('Signup')}>
-              <Text style={styles.inputLabel}>Sign up</Text>
-            </Pressable>
-          </View>
-        }
-      </View>
-        {token &&
-          <View style={styles.button}>
-            <Pressable onPress={handleLogout}>
-              <Text style={styles.inputLabel}>Log out</Text>
-            </Pressable>
-          </View>
-        }
+      <Text>Home screen with map</Text>
     </View>
   );
 }
@@ -81,31 +29,5 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: '600',
     color: '#ffffff',
-  },
-  inputLabel: {
-    padding: 16,
-    fontSize: 18,
-    color: '#ffffff',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#e4d0ff',
-    backgroundColor: "#ADDEF3",
-    color: '#000000',
-    borderRadius: 8,
-    width: '75%',
-    padding: 16,
-  },
-  homeButtonsContainer: {
-    flexDirection: 'row',
-  },
-  button: {
-    width: 120,
-    marginVertical: 24,
-    marginHorizontal: 12,
-    backgroundColor: '#348EC5',
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 });
