95ad88bce87e7a1b1e7d678d50f6351cdf315618
Andy <ajscottlewis@gmail.com>
2023-02-01 09:42:32 +0000

    Add code for retrieving and removing userId (commented out until bug with setting userId resolved on SignupScreen

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -5,13 +5,20 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function HomeScreen({ navigation }) {
   const [token, setToken] = useState(null);
-
+  // const [userId, setUserId] = useState(null);
+  
   useEffect(() => {
     const retrieveToken = async () => {
       const value = await AsyncStorage.getItem('token');
       setToken(value);
     };
     retrieveToken();
+
+    // const retrieveUserId = async () => {
+    //   const value = await AsyncStorage.getItem('user_id');
+    //   setUserId(value);
+    // };
+    // retrieveUserId();
 
     const changedScreen = navigation.addListener('focus', () => {
       retrieveToken();
@@ -23,10 +30,13 @@ function HomeScreen({ navigation }) {
   const handleLogout = async () => {
     await AsyncStorage.removeItem('token');
     setToken(null);
+    // await AsyncStorage.removeItem('user_id');
+    // setUserId(null);
   };
 
   return (
     <View style={styles.globalContainer}>
+      {/* {token && <Text>Logged in as {userId}</Text>} */}
       <Text style={styles.title}>Get Home Safe</Text>
       <View style={styles.homeButtonsContainer}>
         {!token &&
