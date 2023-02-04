c7b7fea7e1e4684f26498519d5f750ed790e9437
williamjones <willj836@gmail.com>
2023-02-04 15:08:06 +0000

    notifications sent when SOS pressed or Home Safe

--- frontend/screens/Map.js
+++ frontend/screens/Map.js
@@ -1,78 +1,182 @@
-import React, { useState, useEffect } from 'react'
-import { Dimensions, StyleSheet, View, Text, Button } from 'react-native'
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
-import MapViewDirections from 'react-native-maps-directions'
-import * as Location from 'expo-location'
-import Constant from 'expo-constants'
-import Search from './Search'
-import Timer from './countdownTimer'
+import React, { useState, useEffect } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import * as Location from "expo-location";
+import Constant from "expo-constants";
+import Search from "./Search";
+import Timer from "./countdownTimer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const GOOGLE_MAPS_APIKEY = 'AIzaSyB01WnR0NuaVmUBTY-897JYHHizmMUc0ek'
+const GOOGLE_MAPS_APIKEY = "AIzaSyB01WnR0NuaVmUBTY-897JYHHizmMUc0ek";
 
 const Map = () => {
-  const [destination, setDestination] = useState(null)
-  const [location, setLocation] = useState(null)
-  const [duration, setDuration] = useState(null)
-  const [distance, setDistance] = useState(null)
-  const [mapRegion, setMapRegion] = useState(null)
-  const [started, setStarted] = useState(false)
-  const [addedTime, setAddedTime] = useState(null)
-  const [isRunning, setIsRunning] = useState(false)
+  const [destination, setDestination] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [distance, setDistance] = useState(null);
+  const [mapRegion, setMapRegion] = useState(null);
+  const [started, setStarted] = useState(false);
+  const [addedTime, setAddedTime] = useState(null);
+  const [isRunning, setIsRunning] = useState(false);
+
   useEffect(() => {
     const getPermissions = async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        return
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
       }
 
-      let currentLocation = await Location.getCurrentPositionAsync({})
-      setLocation(currentLocation)
-      const { width, height } = Dimensions.get('window')
-      const ASPECT_RATIO = width / height
-      const LATITUDE_DELTA = 0.02
-      const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      const { width, height } = Dimensions.get("window");
+      const ASPECT_RATIO = width / height;
+      const LATITUDE_DELTA = 0.02;
+      const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
       setMapRegion({
         latitude: currentLocation.coords.latitude,
         longitude: currentLocation.coords.longitude,
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
-      })
-    }
-    getPermissions()
-  }, [])
+      });
+    };
+    getPermissions();
+  }, []);
 
   useEffect(() => {
     if (destination) {
-      const { latitude, longitude } = location.coords
-      const { lat, lng } = destination
+      const { latitude, longitude } = location.coords;
+      const { lat, lng } = destination;
 
-      const minLat = Math.min(latitude, lat)
-      const maxLat = Math.max(latitude, lat)
-      const minLng = Math.min(longitude, lng)
-      const maxLng = Math.max(longitude, lng)
+      const minLat = Math.min(latitude, lat);
+      const maxLat = Math.max(latitude, lat);
+      const minLng = Math.min(longitude, lng);
+      const maxLng = Math.max(longitude, lng);
 
       setMapRegion({
         latitude: (minLat + maxLat) / 2,
         longitude: (minLng + maxLng) / 2,
         latitudeDelta: maxLat - minLat + 0.015,
         longitudeDelta: maxLng - minLng + 0.015,
-      })
+      });
     }
-  }, [destination, addedTime])
+  }, [destination, addedTime]);
 
   const increaseDuration = (time) => {
-    setDuration((prevDuration) => prevDuration + time)
-  }
+    setDuration((prevDuration) => prevDuration + time);
+  };
 
   const startJourney = () => {
-    setStarted(true)
-    setIsRunning(true)
-  }
+    setStarted(true);
+    setIsRunning(true);
+  };
+
+  const sendSOSNotification = async (userId, data) => {
+    for (const contact of data.emergencyContacts) {
+      await fetch("https://app.nativenotify.com/api/indie/notification", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          subID: `${contact.id}`,
+          appId: 6193,
+          appToken: "rWR1WMqaI8HcWYDUZQFStS",
+          title: `${userId} hit SOS!!!`,
+          message: "Get in touch ASAP!!!",
+        }),
+      });
+      await fetch(
+        `http://192.168.1.110:8080/api/user/notifications/${contact.id}/add`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            notification: {
+              title: `Someone hit SOS!`,
+              message: `${contact.id} hit SOS get in touch!!`,
+              timeSent: new Date(),
+            },
+          }),
+        }
+      );
+    }
+  };
+
+  const sendSafeNotification = async (userId, data) => {
+    for (const contact of data.emergencyContacts) {
+      await fetch("https://app.nativenotify.com/api/indie/notification", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          subID: `${contact.id}`,
+          appId: 6193,
+          appToken: "rWR1WMqaI8HcWYDUZQFStS",
+          title: `${userId} got home safe`,
+          message: "All good",
+        }),
+      });
+      await fetch(
+        `http://192.168.1.110:8080/api/user/notifications/${contact.id}/add`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            notification: {
+              title: `Someone got home safe!`,
+              message: `${contact.id} got home safe`,
+              timeSent: new Date(),
+            },
+          }),
+        }
+      );
+    }
+  };
+
+  const handleSOSbutton = async () => {
+    const userId = await AsyncStorage.getItem("user_id");
+    // got to map through emergency contacts
+    let response = await fetch(
+      `http://192.168.1.110:8080/api/user/contacts/${userId}`
+    );
+    let data = await response.json();
+    if (response.status === 200) {
+      sendSOSNotification(userId, data);
+    }
+  };
+
+  const handleHomeSafe = async () => {
+    const userId = await AsyncStorage.getItem("user_id");
+    let response = await fetch(
+      `http://192.168.1.110:8080/api/user/contacts/${userId}`
+    );
+    let data = await response.json();
+    if (response.status === 200) {
+      sendSafeNotification(userId, data);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
-        <Search setDestination={setDestination} setIsRunning={setIsRunning} setStarted={setStarted}/>
+        <Search
+          setDestination={setDestination}
+          setIsRunning={setIsRunning}
+          setStarted={setStarted}
+        />
       </View>
       <View style={styles.mapContainer}>
         {location && (
@@ -105,10 +209,10 @@ const Map = () => {
                   strokeColor="hotpink"
                   mode="WALKING"
                   onReady={(result) => {
-                    setDistance(result.distance)
-                    setDuration(result.duration)
-                    console.log(`Distance: ${distance} km`)
-                    console.log(`Duration: ${duration} min.`)
+                    setDistance(result.distance);
+                    setDuration(result.duration);
+                    console.log(`Distance: ${distance} km`);
+                    console.log(`Duration: ${duration} min.`);
                   }}
                 />
               </>
@@ -120,57 +224,68 @@ const Map = () => {
         <View style={styles.journeyDetailsContainer}>
           <Text style={styles.journeyDetails}>
             {distance
-              ? `Duration: ${Math.round(
-                  duration,
-                )}mins      Distance: ${Math.round(10*distance)/10}km`
-              : 'Search for a destination to start'}
+              ? `Duration: ${Math.round(duration)}mins      Distance: ${
+                  Math.round(10 * distance) / 10
+                }km`
+              : "Search for a destination to start"}
           </Text>
         </View>
         <View style={styles.bottomContainerChild}>
           <View style={styles.buttons}>
-            <View style={styles.button}>
-              <Button color="white" title="SOS" />
-            </View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                handleSOSbutton();
+              }}
+            >
+              <Text style={{ color: "white" }}>SOS</Text>
+            </TouchableOpacity>
+
             <View style={styles.button}>
               {started ? (
-                <Timer duration={duration} isRunning={isRunning} setIsRunning={setIsRunning}/>
-              ) : (
-                <Button
-                  color="white"
-                  title="Start journey"
-                  onPress={startJourney}
+                <Timer
+                  duration={duration}
+                  isRunning={isRunning}
+                  setIsRunning={setIsRunning}
                 />
+              ) : (
+                <TouchableOpacity onPress={startJourney}>
+                  <Text>Start Journey</Text>
+                </TouchableOpacity>
               )}
             </View>
-            <View style={styles.button}>
-              <Button color="white" title="Home safe" />
-            </View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                handleHomeSafe();
+              }}
+            >
+              <Text style={{ color: "white" }}>Home Safe</Text>
+            </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.bottomContainerChild}>
-          
-        </View>
+        <View style={styles.bottomContainerChild}></View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     flex: 1,
-    width: '100%',
+    width: "100%",
     paddingTop: 40,
-    marginBottom: 10
+    marginBottom: 10,
   },
   searchContainer: {
     height: Constant.statusBarHeight,
-    width: '100%',
+    width: "100%",
     zIndex: 1,
   },
   mapContainer: {
     flex: 3,
-    width: '100%',
+    width: "100%",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
@@ -182,18 +297,18 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     flex: 1,
   },
   button: {
     flex: 1,
-    width: '30%',
-    height: '170%',
-    backgroundColor: '#348EC5',
+    width: "30%",
+    height: "170%",
+    backgroundColor: "#348EC5",
     borderRadius: 15,
     margin: 5,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   journeyDetailsContainer: {
     flex: 0.3,
@@ -202,6 +317,6 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginHorizontal: 12,
   },
-})
+});
 
-export default Map
+export default Map;
