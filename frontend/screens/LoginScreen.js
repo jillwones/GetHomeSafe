4e89e3c428d4f1224bba88fe4012b517c3d68c12
Eliza Thomas <elizamthomas1994@gmail.com>
2023-02-08 13:55:48 +0000

    Update styling for splash screen and login&signup screens

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as React from "react";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { registerIndieID } from "native-notify";
 
 function LoginScreen({ navigation }) {
@@ -11,7 +11,7 @@ function LoginScreen({ navigation }) {
 
   function emailInputHandler(enteredEmail) {
     setEmail(enteredEmail);
-  };
+  }
 
   function passwordInputHandler(enteredPassword) {
     setPassword(enteredPassword);
@@ -19,30 +19,30 @@ function LoginScreen({ navigation }) {
 
   const handleLogin = async () => {
     if (!email) {
-      setError('All fields must be filled');
+      setError("All fields must be filled");
       console.log(error);
       return;
     }
-    
+
     setError(null);
 
-    let response = await fetch('http://localhost:8080/api/user/login', {
-      method: 'POST',
+    let response = await fetch("http://localhost:8080/api/user/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email.toLowerCase(), password: password })
-    })
+      body: JSON.stringify({ email: email.toLowerCase(), password: password }),
+    });
 
-    let data = await response.json()
-    console.log('data: ', data);
-    console.log('token:', data.token);
-    console.log('user_id:', data.user_id);
-    console.log('name: ', data.name);
-    console.log('walkingSpeed: ', data.walkingSpeed);
-    console.log('phoneNumber: ', data.phoneNumber);
-    
-    if(response.status === 200) {
+    let data = await response.json();
+    console.log("data: ", data);
+    console.log("token:", data.token);
+    console.log("user_id:", data.user_id);
+    console.log("name: ", data.name);
+    console.log("walkingSpeed: ", data.walkingSpeed);
+    console.log("phoneNumber: ", data.phoneNumber);
+
+    if (response.status === 200) {
       AsyncStorage.setItem("token", data.token);
       AsyncStorage.setItem("user_id", data.user_id);
       AsyncStorage.setItem("name", data.name);
@@ -51,45 +51,48 @@ function LoginScreen({ navigation }) {
       setEmail(null);
       setPassword(null);
       await registerIndieID(`${data.user_id}`, 6193, "rWR1WMqaI8HcWYDUZQFStS");
-      navigation.replace('NavbarContainer');
+      navigation.replace("NavbarContainer");
     } else {
       setError(data.error);
-      console.log('Error:', error);
+      console.log("Error:", error);
     }
-  }
-  
+  };
+
   return (
     <View style={styles.globalContainer}>
-      <Text style={styles.title}>Login</Text>
-      <Text style={styles.inputLabel}>Email address</Text>
-      <TextInput 
+      <Text style={styles.title}>LOGIN</Text>
+      {/* <Text style={styles.loginButtonText}>Email address</Text> */}
+      <TextInput
         style={styles.textInput}
-        placeholder="Your email here!"
+        placeholder="Email address"
         onChangeText={emailInputHandler}
         value={email}
       />
-      <Text style={styles.inputLabel}>Password</Text>
-      <TextInput 
+      {/* <Text style={styles.loginButtonText}>Password</Text> */}
+      <TextInput
         style={styles.textInput}
-        placeholder="Your password here!"
+        placeholder="Password"
         secureTextEntry={true}
         onChangeText={passwordInputHandler}
         value={password}
       />
-      {error &&
-        <View style={styles.errorContainer}>
-          <Text style={styles.errorText}>{error}</Text>
-        </View>
-      }
-      <View style={styles.button}>
+      <View style={styles.loginButton}>
         <Pressable onPress={handleLogin}>
-          <Text style={styles.inputLabel}>Log in</Text>
+          <Text style={styles.loginButtonText}>LOGIN</Text>
         </Pressable>
         {/* <Button title="Log in" onPress={handleLogin} /> */}
       </View>
-      <Pressable onPress={(() => navigation.navigate('Signup'))}>
-        <Text style={styles.signupButtonText} >Or sign up here</Text>
-      </Pressable>
+      <Text style={styles.orText}>OR</Text>
+      <View style={styles.signupButton}>
+        <Pressable onPress={() => navigation.navigate("Signup")}>
+          <Text style={styles.signupButtonText}>REGISTER</Text>
+        </Pressable>
+      </View>
+      {error && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -99,59 +102,79 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   globalContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingBottom: 60,
-    backgroundColor: "#64C5F0",
+    backgroundColor: "white",
+    position: "relative"
   },
   title: {
     padding: 10,
     fontSize: 22,
-    fontWeight: '600',
-    color: '#ffffff',
+    fontWeight: "600",
+    color: "black",
   },
-  inputLabel: {
-    padding: 16,
+  loginButtonText: {
+    padding: 14,
     fontSize: 18,
-    color: '#ffffff',
+    color: "white",
+    fontWeight: "bold"
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#e4d0ff',
-    backgroundColor: "#ADDEF3",
-    color: '#000000',
+    borderColor: "black",
+    backgroundColor: "#ebe6ef",
+    color: "black",
     borderRadius: 8,
-    width: '75%',
+    width: "75%",
+    margin: 14,
     padding: 16,
   },
   homeButtonsContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
-  button: {
-    width: 120,
-    marginVertical: 24,
+  loginButton: {
+    width: "75%",
+    marginTop: 12,
     marginHorizontal: 12,
-    backgroundColor: '#348EC5',
+    backgroundColor: "black",
     borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  orText: {
+    marginVertical: 16
+  },
+  signupButton: {
+    width: "75%",
+    marginBottom: 12,
+    marginHorizontal: 12,
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
   },
   signupButtonText: {
-    color: '#ffffff',
+    color: "black",
+    fontSize: 16,
+    padding: 14,
   },
   errorContainer: {
+    position: "absolute",
+    width: "75%",
     marginTop: 24,
-    padding: 8,
+    padding: 16,
     borderWidth: 1,
-    borderColor: '#dddddd',
+    borderColor: "red",
     borderRadius: 8,
-    backgroundColor: '#dddddd'
+    bottom: 100
   },
   errorText: {
     fontSize: 18,
-    fontWeight: '600',
-    color: '#D238FF',
+    fontWeight: "600",
+    color: "red",
+    alignSelf: "center",
   },
 });
-
-
